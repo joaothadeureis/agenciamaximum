36eb2ee908c3d871dda396c5b5a3ef9ec3b6bfec
joaothadeureis <thadeureis08@gmail.com>
2023-11-14 17:21:55 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,30 @@
+function toggleClasses() {
+    // Get all buttons and items
+    const buttons = document.querySelectorAll('#show-or-hide-1, #show-or-hide-2, #show-or-hide-3, #show-or-hide-4');
+    const items = document.querySelectorAll('.item-1, .item-2, .item-3, .item-4');
+
+    // Event listener for each button
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            // Remove .active and .visible from all buttons and items
+            buttons.forEach(b => b.classList.remove('active'));
+            items.forEach(item => item.classList.remove('visible', 'visible-mobile'));
+
+            // Add .active to the clicked button and .visible or .visible-mobile to the corresponding item
+            button.classList.add('active');
+            
+            // Check screen size and apply the appropriate class to the corresponding item
+            if (window.matchMedia('(max-width: 767px)').matches) {
+                items[index].classList.add('visible-mobile');
+            } else {
+                items[index].classList.add('visible');
+            }
+        });
+    });
+}
+
+// Initial execution
+toggleClasses();
+
+// Add a listener for screen size changes
+window.addEventListener('resize', toggleClasses);
